8f83964a2a3b92dbab6e73331624fafe1dc9bd49
evgeniy132 <evgeniy132@gmail.com>
2021-01-09 13:57:53 +0200

    ready

---
+++ ExamApp/Data.cs
@@ -0,0 +1,15 @@
+﻿namespace ExamApp
+{
+    public class Data
+    {
+        public Data()
+        {
+            vTimeEnter = 1000;
+            IsRepeat = true;
+        }
+
+        public string vString { get; set; }
+        public int vTimeEnter {get; set;}
+        public bool IsRepeat { get; set; }
+    }
+}
---
+++ ExamApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace ExamApp
+{
+    class Program
+    {
+        private static Data vData = new Data();
+        private static AutoResetEvent vRepeatHandler = new AutoResetEvent(true);
+        private static AutoResetEvent vResetHandler = new AutoResetEvent(true);
+
+
+        public static void Main(string[] args)
+        {
+            vData.vString = Console.ReadLine();
+            Thread vThread = new Thread(vmResetApp);
+            vThread.Start();
+        }
+
+        public static void vmRepeat ()
+        {
+            vRepeatHandler.WaitOne();
+
+            while (vData.IsRepeat)
+            {
+                Console.WriteLine(vData.vString);
+                Thread.Sleep(vData.vTimeEnter);
+            }
+           
+            vRepeatHandler.Set();
+        }
+
+        public static void vmResetApp()
+        {
+            vResetHandler.WaitOne();
+
+            Thread vThread = new Thread(vmRepeat);
+            vThread.Start();            
+
+            Console.ReadLine();
+            vData.IsRepeat = false;
+            vResetHandler.Set();
+
+            vData.vString = Console.ReadLine();
+            vData.IsRepeat = true;
+            Thread vRepeatThread = new Thread(vmResetApp);
+            vRepeatThread.Start();
+        }
+    }
+}
